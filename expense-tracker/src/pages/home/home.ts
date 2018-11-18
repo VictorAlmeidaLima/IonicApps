7416b884d15cdf9a1972003b4a9bff00cfc6381d
VICTOR ALCIDES <victor.alcides@sankhya.com.br>
2018-11-18 15:12:19 -0200

    Initial commit

---
+++ expense-tracker/src/pages/home/home.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  expenses = [
+    {
+      date: '2018-10-30',
+      amount: 10.3,
+      category: 'comida',
+      description: 'almoco'
+    },
+    {
+      date: '2018-11-03',
+      amount: 4.3,
+      category: 'comida',
+      description: 'lanche no final da tarde'
+    },
+    {
+      date: '2018-11-05',
+      amount: 45.3,
+      category: 'comida',
+      description: 'supermercado'
+    }
+  ];
+
+  onItemClick(expense) {
+    console.log('Item clicado', expense);
+    this.navCtrl.push(DetailPage,
+      {expense: expense});
+  }
+
+}
